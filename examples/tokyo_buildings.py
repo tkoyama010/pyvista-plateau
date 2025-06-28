b6615bb90dfe5204588c963deec3d1078aa2b9a9
Your Name <tkoyama010@gmail.com>
2025-06-28 12:03:06 +0900

    feat: add example for loading Tokyo buildings GML file with height filter

--- examples/tokyo_buildings.py
+++ examples/tokyo_buildings.py
@@ -2,6 +2,7 @@ from pyvista_plateau.loader import load_citygml
 from pyvista_plateau.filter import filter_by_height
 from pyvista_plateau.plotter import plot_mesh
 
+# Fix code to load gml file from internet like pyvista load functions AI!
 mesh = load_citygml("Tokyo_23ku_Building.gml")
 filtered = filter_by_height(mesh, min_height=30)
 plot_mesh(filtered, title="Tokyo Buildings taller than 30m")
